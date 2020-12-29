777fddbc31979203cb690ef17433ef41f93bd318
Christian <chrnielsen2003@gmail.com>
2020-12-29 03:46:15 +0100

    top10done

--- app.js
+++ app.js
@@ -6,14 +6,42 @@ fetch("https://forbes400.herokuapp.com/api/forbes400?limit=10")
 	var listOfPeople = document.querySelector(".richPeople")
 
 	data.forEach(people => {
-		console.log(people);
 		const container = document.createElement("article");
 		container.classList.add("personCard")
+		const netWorth = people.estWorthPrev.toFixed(2)
 		container.innerHTML = `
+		
+		<h1>Number ${people.rank}</h1>
 		<p>${people.person.name}</p>
+		<p>Source: ${people.source}</p>
+		<p>Networth: $${netWorth}M</p>
+		<p>Nationality: ${people.countryOfCitizenship}</p>
 		<img src="${people.person.squareImage}">
+		
+		
 		`
 		listOfPeople.appendChild(container)
+		console.log(people);
 	});
+
+
+	setTimeout(function(){  
+
+	function showHide() {
+		if(document.querySelectorAll("showMore").style.display == 'none'){
+document.querySelectorAll("showMore").style.display='block';
+}
+
+else
+{
+document.querySelectorAll("showMore").style.display = 'none';
+}
+	}
+
+	showHide()
+
+}, 1000);
+
+
 
 })
